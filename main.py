f14b411c67d70a5568effc92af64f8cc04ef1dcc
joostzaalberg <joostzaalberg@gmail.com>
2022-09-05 13:28:59 +0200

    create repository

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Joost')
+    print(f'sin(5) using numpy = {np.round(np.sin(5), 3)}')
+
+    print(torch.randn(5,4))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
